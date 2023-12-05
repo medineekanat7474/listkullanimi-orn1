02d89b1613da87afbf482f511df24052eaae65a8
medineekanat7474 <145452232+medineekanat7474@users.noreply.github.com>
2023-12-05 10:25:16 +0300

    Add files via upload

---
+++ listkullanimi-orn1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listkullanimi_orn1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> ogrenciliste = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ogrenciliste.RemoveAt(0);
+            
+            listBox1.DataSource= ogrenciliste.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int elemansayisi = ogrenciliste.Count;
+
+            ogrenciliste.RemoveAt(elemansayisi-1);
+
+            listBox1.DataSource= ogrenciliste.ToList();
+        }
+    }
+}
